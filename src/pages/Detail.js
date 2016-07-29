a757ac867eb6ef1eee4b23d4aa16162bac97f3b8
vivian_xu <540978449@qq.com>
2016-07-29 11:25:41 +0800

    Generating Random Values for Our Page

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Chance from 'chance';
 
 class Detail extends React.Component {
     render() {
-        return <p>{this.props.message}</p>;
+        return <p>hello, {chance.first()} !</p>;
     }
 }
 

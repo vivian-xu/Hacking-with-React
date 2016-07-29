9f8ee9cea52a075e30a2504d5bd24f9fcb44299b
vivian_xu <540978449@qq.com>
2016-07-29 14:58:40 +0800

    Handling Events with JSX: onClick

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,10 +2,16 @@ import React from 'react';
 import Chance from 'chance';
 
 class Detail extends React.Component {
+    buttonClicked() {
+        this.forceUpdate();
+    }
+
     render() {
         return (<div>
             <p> Hello, {chance.first()}!</p>
             <p> You're from {chance.country({full: true})}. </p>
+            <button onClick={this.buttonClicked.bind(this)} >
+            Meet Someone New! </button>
         </div>);
     }
 }

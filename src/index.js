d4dc6998398944fbcf3f151a4729ddf24174037e
vivian_xu <540978449@qq.com>
2016-07-30 23:48:53 +0800

    How to Add a New Route to React Router

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Router, Route, IndexRoute, useRouterHistory, hashHistory } from 'react-router';
 import createHistory from 'history/lib/createHashHistory';
-import {createHashHistory} from 'history';
+// import { Router, Route, IndexRoute, useRouterHistory, hashHistory } from 'react-router';
+// import {createHashHistory} from 'history';
+import { Router, Route, IndexRoute } from 'react-router';
+
 
 import Detail from './pages/Detail';
-
+import List from './pages/List'
 
 ReactDOM.render(
     <Router history={createHistory({ queryKey: false })}
         onUpdate={() => window.scrollTo(0, 0)}>
-        <Route path="/" component={ Detail } />
+
+        <Route path="/" component={ List } />
+        <Route path="/react" component={ Detail } />
     </Router>,
 
     document.getElementById('app')
 );
 
     // <Router history={ useRouterHistory(createHashHistory)({ queryKey: false }) }
-        // onUpdate={ () => window.scrollTo(0, 0) }>
+    //     onUpdate={ () => window.scrollTo(0, 0) }>
---
+++ src/pages/List.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+class List extends React.Component {
+    render() {
+        return <p> This is the list pages.  </p>;
+    }
+}
+
+export default List;
